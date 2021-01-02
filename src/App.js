82efbec45c4f73a04fdcaaabc2622eda005c6589
pariwesh009 <ajay2322020@outlook.com>
2021-01-02 11:56:05 +0530

    class based components and props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { Userform } from "./components/userform/userform";
 function App() {
   return (  //JSX
     <div className="App">
-      <Userform label='First Name'></Userform>
-      <Userform label='Last Name'></Userform>
-      <Userform label='age'></Userform>
+      <Userform label='First Name' color='cyan'></Userform>
+      
 
     </div>
   );
--- src/components/userform/userform.js
+++ src/components/userform/userform.js
@@ -1,7 +1,25 @@
+import React from 'react';
 
+// export function Userform (props){
+//     return (
+//         <input placeholder={props.label} style={{background:props.color}}/>
+//     )
+// }
 
-export function Userform (props){
-    return (
-        <input placeholder={props.label}/>
-    )
+export class Userform extends React.Component {
+    save(event){
+        console.log(event);
+    }
+    render() {
+        return (
+            <div>
+                <input placeholder={this.props.label} style={{ background: this.props.color }} />
+                <input placeholder='Last Name' style={{ background: this.props.color }} />
+                {/* <button onClick={(event)=>{
+                    console.log(event);
+                }}>Save</button> */}
+                 <button onClick={this.save}>Save</button>
+            </div>
+        )
+    }
 }
