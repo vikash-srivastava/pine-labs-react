a038fea020178f210e01b8fe941d94da78eaa1e0
Vikash Srivastava <srivastava.vikash61@gmail.com>
2021-01-11 12:07:23 +0530

    backend-service integration with ui components

--- src/backend-service.js
+++ src/backend-service.js
@@ -1,7 +1,23 @@
 import jquery from "jquery";
 
+const SERVER_BASE_URL = "http://localhost:4200/";
+const USER_SERVICE_CONTEXT = "users/";
+const ROLE_SERVICE_CONTEXT = "roles/";
 export class BackendService{
     static saveUser(user, success){
-        return jquery.post("http://localhost:4200/users/", user, success);
+        return jquery.post(SERVER_BASE_URL + USER_SERVICE_CONTEXT, user, success);
+    }
+    static deleteUser(userId, successCallback){
+        return jquery.ajax(SERVER_BASE_URL + USER_SERVICE_CONTEXT + userId, {
+            type : 'delete',
+            success: successCallback
+        });
+    }
+    static loadUser(){
+        return jquery.get(SERVER_BASE_URL + USER_SERVICE_CONTEXT);
+    }
+
+    static loadRoles(){
+        return jquery.get(SERVER_BASE_URL + ROLE_SERVICE_CONTEXT);
     }
 }
--- src/components/userform/userform.js
+++ src/components/userform/userform.js
@@ -2,56 +2,95 @@ import React from 'react';
 import { BackendService } from '../../backend-service';
 import './userform.css';
 export class Userform extends React.Component {//MVC
-    constructor() { //only one 
+    constructor() { 
         super();
-        this.state = { //model
+        this.state = { 
             user: {
-                fname: 'Pariwesh1',
-                age: 30,
+                fname: 'Vikash',
+                lname: 'Srivastava',
+                age: 32,
+                salary:20000,
+                gender: 'M'
             },
-            users: []
+            users: [],
+            roles: []
         }
-    }//ES6
+        BackendService.loadUser().done((response)=>{
+            console.log("load user success");
+            this.setState({
+                users: response
+            }); 
+        }).fail((response)=>{
+            console.log("load users failed");           
+        });
+        BackendService.loadRoles().done((response)=>{
+            console.log("load roles success");
+            this.setState({
+                roles: response
+            }); 
+        }).fail((response)=>{
+            console.log("load roles failed");           
+        });
+    }
     save = (event) => {
-        BackendService.saveUser(this.state.user, 
-            (response)=>{ //success callback functions
-                this.setState({  //to rerender, call setState
-                    users: [...this.state.users, Object.assign({}, this.state.user)]
+        BackendService.saveUser(this.state.user,
+            (response)=>{
+                console.log(response);
+                //this.state.users.push(this.state.user); 
+                this.setState({ //to render on UI, always set state through setState method
+                    users: [...this.state.users, response]
+                    //users: this.state.users;
                 });
             }).fail((error)=>{
-                alert('Somemthing went wrong, please retry..');
-            });
+                console.log("Oops!!! something went wrong.", error);
+            });        
     }
     handleEvent = (event) => {
-        this.setState({  //to rerender, call setState
+        this.setState({  
             user: Object.assign(this.state.user, { [event.target.name]: event.target.value })
         });
     }
-    deleteUser = function(index, secodnArg)  {
+    deleteUser = function(index, userId)  {
         const decision = window.confirm('Are you sure??');
         if(!decision){
             return;
         }
+        BackendService.deleteUser(userId, (response) => {
+            this.state.users.splice(index, 1);
+            this.setState({
+                users: this.state.users 
+            });                                   
+        }).fail((response)=>{
+            console.log("Oops!!! Something went wrong", response);
+        });
         console.log(decision);
         console.log(this); 
-        console.log(index, secodnArg);
-        this.state.users.splice(index, 1);
-        this.setState({
-            users: this.state.users 
-        })
+        //console.log(index, secodnArg);
+        
     }
+
+        
     render() {
         const userModel = this.state.user;
         return (
-            <div>
+            <div onLoad={this.loadUser}>
                 <input value={userModel.fname} name='fname' onChange={this.handleEvent} placeholder={this.props.label} style={{ background: this.props.color }} />
-                <input value={userModel.age} name='age' onChange={this.handleEvent}
-                    placeholder='first Name copy' style={{ background: this.props.color }} />
-                <input placeholder='salary' value={userModel.salary} onChange={this.handleEvent} name='salary'></input>
+                <input value={userModel.lname} name='lname' onChange={this.handleEvent} placeholder={this.props.label} style={{ background: this.props.color }} />
+                <input value={userModel.age} name='age' onChange={this.handleEvent} placeholder='Age' style={{ background: this.props.color }} />
+                <input value={userModel.salary} name='salary' onChange={this.handleEvent} placeholder='salary'></input>
+                <input value='M' name='gender' onChange={this.handleEvent} type="radio" />Male
+                <input value='F' name='gender' onChange={this.handleEvent} type="radio"/>Female
+                <div>Role : 
+                    {this.state.roles.map( (role) =>{
+                        return <div><input value={role} name='role' onChange={this.handleEvent} type="radio"/>{role}</div>
+                    })
+                    }                   
+                 </div>   
                 <button onClick={this.save}>Save</button>
                 <table>
                     <thead >
                         <th>First Name</th>
+                        <th>Last Name</th>
                         <th> Age</th>
                         <th> Salary</th>
                     </thead>
@@ -59,9 +98,12 @@ export class Userform extends React.Component {//MVC
                         {this.state.users.map( (user, index)=> {
                             return <tr>
                                 <td>{user.fname}</td>
+                                <td>{user.lname}</td>
                                 <td>{user.age}</td>
                                 <td>{user.salary}</td>
-                                <td><button onClick={this.deleteUser.bind(this, index, 34)}>Delete</button></td>
+                                <td>{user.gender}</td>
+                                <td>{user.role}</td>
+                                <td><button onClick={this.deleteUser.bind(this, index, user.id)}>Delete</button></td>
                             </tr>
                         })}
                     </tbody>
